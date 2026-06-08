2c924798df0d15fa6a2b4076ed5dfe50ddad6116
Majid Qurashi <106837825+majid-qurashi@users.noreply.github.com>
2026-06-08 22:21:47 +0530

    Implement Euler's Totient function

---
+++ Eulers_totient.py
@@ -0,0 +1,20 @@
+# Majid Qurashi - 220365
+
+def phi(n):
+    result = n
+
+    p = 2
+    while p * p <= n:
+        if n % p == 0:
+            while n % p == 0:
+                n //= p
+            result -= result // p
+        p += 1
+
+    if n > 1:
+        result -= result // n
+
+    return result
+
+n = int(input("Enter n: "))
+print("Phi(", n, ") =", phi(n))
